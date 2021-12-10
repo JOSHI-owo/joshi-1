4ea8fc96c4de0a228b1079d5d3e1e9d6557063c0
JOSHI-owo <ajithpkumar231@gmail.com>
2021-12-10 17:29:51 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import './App.css';
 import Home from './components/Home';
 import About from './components/About'
 import Service from './components/Service';
 import Contact from './components/Contact';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
+    <>
+    <Navbar/>
     <Switch>
       <Route path="/home" component={Home}  />
       <Route path="/about" component={About}/>
@@ -16,6 +21,8 @@ function App() {
       <Redirect to="/"/>
       
     </Switch>
+
+    </>
       );
   
 }
