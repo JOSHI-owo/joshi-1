1890c843e59e26ecc9b7b5371e181dd96164239f
JOSHI-owo <ajithpkumar231@gmail.com>
2021-12-21 14:49:48 +0530

    navbar and home edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,28 @@
-import React from 'react';
-import { Switch, Route, Redirect } from 'react-router';
+import React from "react";
+import { Switch, Route, Redirect } from "react-router";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
-import './App.css';
-import Home from './components/Home';
-import About from './components/About'
-import Service from './components/Service';
-import Contact from './components/Contact';
-import Navbar from './components/Navbar';
+import "./App.css";
+import "./index.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import Service from "./components/Service";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Switch>
-      <Route path="/home" component={Home}  />
-      <Route path="/about" component={About}/>
-      <Route path="/contact" component={Contact}/>
-      <Route path="/service" component={Service}/>
-      <Redirect to="/"/>
-      
-    </Switch>
-
+      <Navbar />
+      <Switch>
+        <Route path="/home" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/service" component={Service} />
+        <Redirect to="/" />
+      </Switch>
     </>
-      );
-  
+  );
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
@@ -8,9 +9,9 @@ function Navbar() {
           <div className="col-10 mx-auto">
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
               <div className="container-fluid">
-                <a className="navbar-brand" href="#">
+                <NavLink className="navbar-brand" to="/">
                   Navbar
-                </a>
+                </NavLink>
                 <button
                   className="navbar-toggler"
                   type="button"
@@ -26,71 +27,45 @@ function Navbar() {
                   className="collapse navbar-collapse"
                   id="navbarSupportedContent"
                 >
-                  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                      <a
-                        className="nav-link active"
+                      <NavLink
+                        activeClassName="menu_active"
+                        className="nav-link "
                         aria-current="page"
-                        href="#"
+                        to="/home"
                       >
                         Home
-                      </a>
+                      </NavLink>
                     </li>
                     <li className="nav-item">
-                      <a className="nav-link" href="#">
-                        Link
-                      </a>
-                    </li>
-                    <li className="nav-item dropdown">
-                      <a
-                        className="nav-link dropdown-toggle"
-                        href="#"
-                        id="navbarDropdown"
-                        role="button"
-                        data-bs-toggle="dropdown"
-                        aria-expanded="false"
+                      <NavLink
+                        activeClassName="menu_active"
+                        className="nav-link"
+                        to="/service"
                       >
-                        Dropdown
-                      </a>
-                      <ul
-                        className="dropdown-menu"
-                        aria-labelledby="navbarDropdown"
-                      >
-                        <li>
-                          <a className="dropdown-item" href="#">
-                            Action
-                          </a>
-                        </li>
-                        <li>
-                          <a className="dropdown-item" href="#">
-                            Another action
-                          </a>
-                        </li>
-                        <li>
-                          <hr className="dropdown-divider" />
-                        </li>
-                        <li>
-                          <a className="dropdown-item" href="#">
-                            Something else here
-                          </a>
-                        </li>
-                      </ul>
+                        Services
+                      </NavLink>
                     </li>
                     <li className="nav-item">
-                      <a className="nav-link disabled">Disabled</a>
+                      <NavLink
+                        activeClassName="menu_active"
+                        className="nav-link"
+                        to="/about"
+                      >
+                        About
+                      </NavLink>
+                    </li>
+                    <li className="nav-item">
+                      <NavLink
+                        activeClassName="menu_active"
+                        className="nav-link"
+                        to="/contact"
+                      >
+                        Contact
+                      </NavLink>
                     </li>
                   </ul>
-                  <form className="form d-flex">
-                    <input
-                      className="form-control me-2"
-                      type="search"
-                      placeholder="Search"
-                      aria-label="Search"
-                    />
-                    <button className="btn button btn-outline-success " type="submit">
-                      Search
-                    </button>
-                  </form>
                 </div>
               </div>
             </nav>
